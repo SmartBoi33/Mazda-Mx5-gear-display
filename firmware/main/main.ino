e22ca7ab1c36aaf91a3b18790679968958ac1bae
aricma <adrian@aricma.com>
2024-10-05 23:04:42 +0200

    change main to allow for a basic toggle between calibration and gear detection mode

--- firmware/main/main.ino
+++ firmware/main/main.ino
@@ -21,18 +21,23 @@ void setup() {
 
 // MAIN LOOP
 void loop() {
+    bool isInCalibraitionMode = digitalRead(A4);
     int sensors[NUM_SENSORS] = {
         analogRead(A0),
         analogRead(A3),
         analogRead(A1),
         analogRead(A2)
     };
+    if (isInCalibraitionMode) {
+        // calibrate(sensors, gear_mappings);
+        Serial.println("C");
+    } else { // Gear Detection Mode
+        char next_gear = map_sensor_values_to_gear(sensors, gear_mappings);
 
-    char next_gear = map_sensor_values_to_gear(sensors, gear_mappings);
-
-    if (should_rerender(current_gear, next_gear)) {
-        current_gear = next_gear;
-        Serial.println(current_gear);
+        if (should_rerender(current_gear, next_gear)) {
+            current_gear = next_gear;
+            Serial.println(current_gear);
+        }
     }
 
     delay(delay_for_refresh_rate);
