3e62f8873285021b116fca90773e5efd19f04c01
aricma <adrian@aricma.com>
2025-03-15 22:43:10 +0100

    move the gear mapping related types into the gear mapping module

--- firmware/main/gear.h
+++ firmware/main/gear.h
@@ -1,18 +1,7 @@
 #ifndef GEAR_H
 #define GEAR_H
 
-struct SensorRange {
-    int min;
-    int max;
-};
-
-#define NUM_SENSORS 4
-#define NUM_GEARS 9
-
-struct SensorToGearMapping {
-    char gear;
-    SensorRange sensor_ranges[NUM_SENSORS];
-};
+#include "gear_mappings.h"
 
 char map_sensor_values_to_gear(const int sensor_values[NUM_SENSORS], const SensorToGearMapping mappings[NUM_GEARS]);
 bool is_within_range(int value, SensorRange range);
--- firmware/main/gear_mappings.h
+++ firmware/main/gear_mappings.h
@@ -1,7 +1,19 @@
 #ifndef GEAR_MAPPINGS_H
 #define GEAR_MAPPINGS_H
 
-#include "gear.h"
+#include "sensors.h"
+
+#define NUM_GEARS 9
+
+struct SensorRange {
+    int min;
+    int max;
+};
+
+struct SensorToGearMapping {
+    char gear;
+    SensorRange sensor_ranges[NUM_SENSORS];
+};
 
 // Gear sensor mappings
 extern SensorToGearMapping gear_mappings[NUM_GEARS];
